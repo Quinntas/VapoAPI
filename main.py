c8076fcfc21c9d43938b9b9c35c1c98ff10d9887
Caio Quintas <caioquintassantiago@gmail.com>
2022-12-14 09:31:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse, FileResponse
 from starlette.middleware.sessions import SessionMiddleware
 
+from src.python.routers.v1.store import store
 from src.python.services.response import json_response
 
 SESSION_SECRET = config('SESSION_SECRET')
@@ -93,14 +94,11 @@ async def add_process_time(request: Request, call_next):
 
 app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET)
 
-if API_STATE == 'Development':
-    from src.python.routers.v1.store import store
-
-    app.include_router(
-        store,
-        prefix="/api/v1/store",
-        tags=["store"]
-    )
+app.include_router(
+    store,
+    prefix="/api/v1/store",
+    tags=["store"]
+)
 
 
 def main():
