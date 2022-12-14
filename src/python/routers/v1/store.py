cd2a8f2a1cb78a2b571539331c1d8bd13d33d30c
Caio Quintas <caioquintassantiago@gmail.com>
2022-12-14 09:27:28 -0300

    testing

--- src/python/routers/v1/store.py
+++ src/python/routers/v1/store.py
@@ -7,7 +7,6 @@ from fastapi import APIRouter
 from src.python.services.response import json_response
 
 store = APIRouter()
-refreshing = False
 last_refresh = time.time()
 
 
@@ -19,11 +18,10 @@ def check_last_refresh(current_time: float):
 
 @store.get("/refresh")
 async def refresh():
-    global refreshing
+    global last_refresh
+
     if check_last_refresh(last_refresh):
-        refreshing = False
-    if refreshing is False:
-        refreshing = True
+        last_refresh = time.time()
         subprocess.Popen("python src/python/api/refresh.py", stdout=subprocess.PIPE, shell=False)
     return json_response(
         {
